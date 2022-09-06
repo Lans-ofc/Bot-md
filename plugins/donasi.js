46eeab41c7ecce9bc2d59d980b63909a51f128c8
Meicantik12 <106010004+Meicantik12@users.noreply.github.com>
2022-09-06 10:36:52 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • pulsa [083132749084]
+│ • Gopay  [083132749084]
+│ • Dana  [083132749084]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6283132749084
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
