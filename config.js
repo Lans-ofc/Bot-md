16080a7f539782fe21a8157e800794b8ace3a494
Meicantik12 <106010004+Meicantik12@users.noreply.github.com>
2022-09-24 02:02:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -66,8 +66,11 @@ global.zt = '*'
 global.zc = ''
 
 global.thumbnailUrl = [
-  'https://telegra.ph/file/588da98d3eda2097c64dc.jpg', 'https://telegra.ph/file/c0987be33ca1a5865b7d1.jpg',
-  'https://telegra.ph/file/6ad25cde80b5f4f591c65.jpg', 'https://telegra.ph/file/466c3993f0a56192ac07d.jpg',
+  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
+  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
+  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
+  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
+  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
 ]
 
 //============= Games ================//
