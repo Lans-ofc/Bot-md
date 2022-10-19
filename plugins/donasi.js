1b5c2d21594200ed364096a1728748a97f8e2f62
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2022-10-19 20:57:41 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • pulsa [083132749084]
+│ • pulsa [082161152073]
 │ • Gopay  [083132749084]
-│ • Dana  [083132749084]
+│ • Dana  [082161152073]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6283132749084
+│ > Ingin donasi? Wa.me/6282161152073
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
